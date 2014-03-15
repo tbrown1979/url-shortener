7d274cd1ecafe7b400e8ea08b1b21ae1122f45f7
tbrown1979 <tbrown@uni.edu>
2014-03-15 13:22:18 -0600

    Connected with redis. Successfully retrieved info.

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,39 @@
  */
 
 if (process.env.REDISTOGO_URL) {
-    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-    var redis = require("redis").createClient(rtg.port, rtg.hostname);
-
-    redis.client.auth(rtg.auth.split(":")[1]);
+  console.log("cloud")
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var client = require("redis").createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(":")[1]);
 } else {
-    var redis = require("redis").createClient();
+  console.log("local")
+  var client = require("redis").createClient();
+}
+
+function statusResponse(status, msg) {
+  return { "status" : status, "message" : msg }
 }
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.test = function(req, res){
+  var accessedUrl = req.params.test;
+  console.log(accessedUrl);
+  client.get(accessedUrl,
+    function(err, reply) { 
+      if (err) { 
+        res.json(statusResponse("failed", err)); 
+      } else {
+        if (reply === null) {
+          res.json(statusResponse("success", "need to actually do stuff"));
+        } else {
+          res.json(statusResponse("success", reply));
+        }
+      }
+    }
+  );
+
+}
+
